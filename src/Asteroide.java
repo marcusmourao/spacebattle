92b6dc7bec5f35bbc50c497f0cfe47c0f0ff76ec
marcus <marcus-pf@hotmail.com>
2017-01-02 16:23:33 -0200

    Criação da classe Asteroide

---
+++ src/Asteroide.java
@@ -0,0 +1,18 @@
+/**
+ * Created by marcus on 02/01/17.
+ */
+public class Asteroide {
+    int vida;
+
+    public Asteroide(int vida) {
+        this.vida = vida;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+}
