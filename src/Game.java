cad3963c7973816196cfe7b8dcb2a5b088a5c3f9
marcus <marcus-pf@hotmail.com>
2017-02-04 12:03:10 -0200

    Create 3d branch

--- src/Game.java
+++ src/Game.java
@@ -66,31 +66,86 @@ public class Game extends BasicGameState {
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
         background.draw(0,0);
+        float angle = 0;
 
-        if(!collision_nave) {
-            textureNave.draw(nave.getX(), nave.getY());
+        while (!Display.isCloseRequested()) {
+            // Clear the screen and depth buffer
+            GL11.glMatrixMode(GL11.GL_PROJECTION);
+            GL11.glLoadIdentity();
+            GL11.glOrtho(0, 800, 600, 0, 800, -600);
+            GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
+            GL11.glPushMatrix();
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+            GL11.glLoadIdentity();
+            glShadeModel(GL_FLAT);
+            GL11.glTranslatef(400,300,0.0f);
+            GL11.glRotatef(angle, 0.0f, 0.5f, 0.0f);
+            GL11.glRotatef(50, 0.0f, 0.0f, 1.0f);
+            GL11.glScalef(100,100,100);
+            GL11.glBegin(GL11.GL_QUAD_STRIP);
+            {
+
+                //this Makes a box that has no top or bottom.
+                //Front - Orange
+                //Right - White
+                //Back - Blue
+                //Left - Teal
+                glColor4f(1.0f,0.5f,0.0f,1.0f);
+                glVertex3f(0.0f,1.0f,0.0f);	//Front Bottom Left
+                glVertex3f(0.0f,0.0f,0.0f);	//Front Top Left
+                glVertex3f(1.0f,1.0f,0.0f);	//Front Bottom Right
+                glVertex3f(1.0f,0.0f,0.0f);	//Front Top Right
+
+                glColor4f(1.0f,1.0f,1.0f,1.0f);
+                glVertex3f(1.0f,1.0f,1.0f);	//Right Bottom Back
+                glVertex3f(1.0f,0.0f,1.0f);	//Right Top Back
+
+
+                glColor4f(0.0f,0.0f,1.0f,1.0f);
+                glVertex3f(0.0f,1.0f,1.0f);	//Back left Bottom
+                glVertex3f(0.0f,0.0f,1.0f);	//Back Left Top
+
+
+                glColor4f(0.0f,1.0f,1.0f,1.0f);
+                glVertex3f(0.0f,1.0f,0.0f);	//Left Bottom Front
+                glVertex3f(0.0f,0.0f,0.0f);	//Left Top Front
+
+            }
+            GL11.glEnd();
+            GL11.glPopMatrix();
+            angle +=0.5f;
+            System.out.println(angle);
+
+            Display.update();
+
         }
 
-        if(!asteroide.isEmpty()){
-            for(int i=0; i<asteroide.size(); i++)
-                textureAsteroide.draw(asteroide.get(i).getX(),asteroide.get(i).getY());
-        }
 
-        if(!municao.isEmpty()){
-            for(int i=0; i<municao.size();i++)
-                textureBala.draw(municao.get(i).getX(),municao.get(i).getY());
-        }
-
-        g.drawString("Pontuacao: "+pontuacao,10,10);
-        String vida = "Vida: ";
-        for (int i = 0; i < nave.getVida(); i++)
-            vida += "*";
-        g.drawString(vida, 10, 35);
-
-        // Verifica se o jogador morreu
-        if (nave.getVida() <= 0) {
-            g.drawString("Game Over", 350, 290);
-        }
+//        if(!collision_nave) {
+//            textureNave.draw(nave.getX(), nave.getY());
+//        }
+//
+//        if(!asteroide.isEmpty()){
+//            for(int i=0; i<asteroide.size(); i++)
+//                textureAsteroide.draw(asteroide.get(i).getX(),asteroide.get(i).getY());
+//        }
+//
+//        if(!municao.isEmpty()){
+//            for(int i=0; i<municao.size();i++)
+//                textureBala.draw(municao.get(i).getX(),municao.get(i).getY());
+//        }
+//
+//        g.drawString("Pontuacao: "+pontuacao,10,10);
+//        String vida = "Vida: ";
+//        for (int i = 0; i < nave.getVida(); i++)
+//            vida += "*";
+//        g.drawString(vida, 10, 35);
+//
+//        // Verifica se o jogador morreu
+//        if (nave.getVida() <= 0) {
+//            g.drawString("Game Over", 350, 290);
+//        }
     }
 
     // update-method with all the magic happening in it
